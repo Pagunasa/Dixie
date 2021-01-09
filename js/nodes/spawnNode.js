2bce34c1d3f77206bde6ac222bb6aba7ba66fa95
Pagunasa <guillem.martinez1996@gmail.com>
2021-01-09 12:22:29 +0100

    Update spawnNode.js

--- js/nodes/spawnNode.js
+++ js/nodes/spawnNode.js
@@ -1,12 +1,16 @@
-function mySpawnNode() {
+function mySpawnNode() 
+{
 	this.properties = {
+		id: 0,
         max_particles: 100,
         spawn_rate: 10,
         position: new Float32Array(3),
         mode: "Point"
     };
    
-    var that = this;
+    this.last_status = {
+    	max_particles: 100
+    };
 
 	this.addWidget("combo", "Mode", "Point", function(){}, { values:["Point", "Mesh", "2D Geometry"] });
 
@@ -17,8 +21,31 @@ function mySpawnNode() {
 	this.addOutput("Particle system", "particle_system");
 }
 
-mySpawnNode.title = "Spawn Particles";
+mySpawnNode.prototype.onAdded = function() 
+{
+	this.properties.id = this.id;
+	createMesh(this.id, this.properties.max_particles);
+	system_list.push(new SystemInfo(this.id));
+};
 
-mySpawnNode.prototype.onExecute = function() {
+mySpawnNode.prototype.onExecute = function() 
+{
+	this.properties.max_particles = this.getInputData(0) || 100;
+	this.properties.spawn_rate    = this.getInputData(1) || 10;
+	this.properties.position      = this.getInputData(2) || new Float32Array(3);
 
+	if (this.properties.max_particles != this.last_status.max_particles)
+	{
+		this.last_status.max_particles == this.properties.max_particles;	
+		resizeBufferArray(this.id, searchMesh(this.id), this.properties.max_particles);
+	}
+
+	this.setOutputData(0, this.properties);
 }
+
+mySpawnNode.prototype.onRemoved = function(){
+	searchMesh(this.id, true);
+    searchSystem(this.id, true);
+}
+
+mySpawnNode.title = "Spawn Particles";
