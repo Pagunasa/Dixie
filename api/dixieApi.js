b626db75133ade503ab832d2d74ca91094f41a18
Pagunasa <guillem.martinez1996@gmail.com>
2021-04-11 13:09:11 +0200

    API Textures

--- api/dixieApi.js
+++ api/dixieApi.js
@@ -506,7 +506,7 @@ class DixieParticleSystem {
 			}
 
 			//LOAD TEXTURE ATLAS
-			c_texture_loader_f_(directory_+"/"+this.atlasName, this.atlas);
+			c_texture_loader_f_(directory_+"/atlas/"+this.atlasName, this.atlas);
 		}
 
 		this.renderInfo = {}
